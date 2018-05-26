6098ae28fe90c9cca19a8ad79125614031fa91a8
guilhermefsilva <37852543+guilhermefsilva@users.noreply.github.com>
2018-05-25 22:38:30 -0300

    Add files via upload

--- P7.c
+++ P7.c
@@ -1,27 +1,32 @@
-//Nome: Guilherme Felipe da Silva
-//Matricula: 11811EEL039
+//Nome:Guilherme Felipe da Silva
+//Matricula:11811EEL039
+
 #include <stdio.h>
  
-int A(int m, int n) {
-	if (m == 0)
-		return n + 1;
-  
-	if ((n == 0)&&(m>0))
-		return A(m - 1, 1);
-  
-	if ((n>0)&&(m>0))
-		return A(m - 1, A(m, n - 1));
-}
+int A(int , int );
+
 int main() {
 	
 	int m,n;
 	
-	printf("Digite o valor para m: ");
+	printf("Digite o valor de m: ");
 	scanf("%d", &m);
 	printf("Digite o valor de n: ");
 	scanf("%d", &n);
 	printf("%d", A(m, n));
     
   }
-
+  
+int A(int m, int n) {
+	
+	if (m == 0)
+		return n + 1;
+		
+	if ((n == 0)&&(m>0))
+		return A(m - 1, 1);
+		
+	if ((n>0)&&(m>0))
+		return A(m - 1, A(m, n - 1));
+		
+}
  
