22a007b882120845c9aa7fe96eb4c0f8f18e3a1b
guilhermefsilva <37852543+guilhermefsilva@users.noreply.github.com>
2018-06-21 09:24:21 -0300

    Update P4.c

--- P4.c
+++ P4.c
@@ -5,7 +5,7 @@ int main(){
 	int i=0,j=0;
 	char num[256];
 	printf("Digite um conjunto com caracteres: ");
-	scanf("%s",&num);getchar();
+	scanf("%s",num);getchar();
 	printf("Os numeros presente no conjunto de caracteres sao: ");
 	while(num[i]!='\0'){
 		if(num[i]>='0' && num[i]<='9'){
