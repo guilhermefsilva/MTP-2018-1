af9af275081d722fe8d4e25af4351f7ec40f18ce
guilhermefsilva <37852543+guilhermefsilva@users.noreply.github.com>
2018-06-23 00:55:33 -0300

    Add files via upload

--- P5.c
+++ P5.c
@@ -1,44 +1,37 @@
 //Nome: Guilherme Felipe da Silva
 //Matrícula: 11811EEL039
+
 #include<stdio.h>
 int main(){
-	int o;
+    char str[256];
+	int o,j;
 	printf("1. Codificar\n2. Decodificar:\n");
 	scanf("%d",&o);getchar();
 	if(o==1){
-		char str[256];
-		int *pc,i=0;
-		
+		int *pc;
+
 		printf("O que quer codificar: ");
-		gets(str);
-		fflush(stdin);
-		
+		fgets(str,256,stdin);
 		printf("A palavra codificada e: ");
-		for(pc=(int*)str; pc < ((int *) str) + sizeof(str); pc++){
-			if(*pc==0)
-				break;
-			printf("%d, ",*pc);
+		for(j=0; str[j]!='\0'; j+=4){
+			pc = (int *) &str[j];
+			if(str[j+4]!='\0')
+				printf("%d, ", *pc);
+			else
+				printf("%d", *pc);
 		}
-		
+
 	}
 	if(o==2){
-		char *pd;
-		int vet[10];
-		int j,n=0;
+		char vir;
+		int vet[64];
 		printf("O que quer decodificar: ");
-		for(j=0;j<sizeof(char);j++){
-			scanf("%d, ",&vet[j]);
-			if(vet[j]==10)
-			break;
-			getchar();
+		for(j=0;j<64;j++){
+			scanf("%d%c",&vet[j],&vir);
+			if(vir!=',')
+                break;
+
 		}
-		for(n;n<sizeof(char);n++)
-		pd=((char*)&vet[n]);
-		for(;pd<(char*)&vet+sizeof(vet);pd++){
-			if(*pd==0)
-			break;
-			printf("%c",*pd);
-		}
-		printf("%d",vet[1]);
+        printf("%s",&vet);
 	}
 }
